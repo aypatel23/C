674a81520efd70960b68e6dd5a4f6568af1fbba2
Ayushi patel <aypatel@redhat.com>
2025-03-05 17:07:45 -0500

    Succesfully included if else loop but still need to work on while loop

---
+++ chapter1/ex6.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/* copy input to output; 2nd version*/
+int main(void)
+{
+
+/*
+	c = 5;
+	if (c == 4) {  // returns 0 or 1
+		printf("true c equals 4\n");
+	}
+	else if (c == 5) {
+		printf("true c equals 5\n");
+	}
+	else {
+		printf("nothing is true\n");
+	}
+*/
+
+
+
+/*	//while ((c = getchar()) != EOF)  // while has to 0 or 1
+*/
+
+
+	int c;
+	int ret;
+
+	while {((c = getchar()) != EOF);  // while has to 0 or 1
+
+	//while ((c = getchar()) )
+
+
+	c = getchar()
+        putchar(c)
+	/* ret = c != EOF;  // != has to be 0 or 1
+
+		printf("Value of return is %d\n", ret); */ 
+
+	if (ret == 0){
+		printf ("Return is false \n");
+	}
+	else if (ret == 1){
+		printf ("Return is true \n");
+	}
+	else {
+		printf ("Return is invalid \n");
+	}
+
+/*	for extra credit put the if/else check back in a while loop
+*/
+
+}
