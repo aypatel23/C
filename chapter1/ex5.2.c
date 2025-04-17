0c137d1efdc5a78a81f2387bed4730c6168bde9c
Ayushi patel <aypatel@redhat.com>
2025-04-17 15:29:12 -0400

    Removed the extra practice binary & compiler files: ex5.1.c ex5.11 ex5.2.c ex6.1

--- chapter1/ex5.2.c
+++
@@ -1,7 +0,0 @@
-/* temp conversion table in reverse order, 300 to 0 */
-#include <stdio.h>
-int main(void){
-int fahr;
-for (fahr=300; fahr>=0; fahr=fahr-20)
-printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-}
