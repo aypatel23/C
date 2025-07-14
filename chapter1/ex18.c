bb02c8a0a2808105c3db0da7dbcc824abe6763ff
Ayushi patel <aypatel@redhat.com>
2025-07-14 16:39:23 -0400

    Remove the trailing blanks and tabs from each input line

---
+++ chapter1/ex18.c
@@ -0,0 +1,45 @@
+/* Write a Program to remove the trailing blanks and tabs from each input line and to delete entirely blank lines */
+
+#include<stdio.h>
+
+#define MAXLINE 1000
+
+int getaline(char line[], int lim);
+int remove_trail(char rline[]);
+
+int main(void)
+{
+	int len;
+	char line[MAXLINE];
+
+	while ((len = getaline(line, MAXLINE)) > 0)//if (remove_trail(line) > 0)
+		printf("%s", line);
+}
+
+// declare getaline
+
+int getaline(char line[], int lim)
+{
+	int i, c;
+
+	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+
+	if (c == '\n') {
+		if (line[i-1] == '\t' || line[i-1] == ' ') {
+			// inputs "cat"
+			// line = ['c', 'a', 't', '\t', '\n']
+			line[i-1] = 'X'; /* Make it clear on screen what changed */
+		}
+
+		line[i] = c;
+		++i;
+	}
+
+	line[i] = '\0';
+
+	// blank lines
+	if (line[1] == '\0')
+		i = 0;
+	return i;
+}
