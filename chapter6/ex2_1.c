cb81a12475141426c328906f4d146a72907fe27d
Ayushi patel <aypatel@redhat.com>
2026-05-20 10:50:29 -0400

    Chapter 6 : keyword-counting program by using pointers instead of Arrays.

---
+++ chapter6/ex2_1.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAXWORD 100
+#define BUFSIZE 100 /* buffer size for ungetch */
+#define NKEYS (sizeof(keytab) / sizeof(keytab[0])) /* # of words in keytab */
+
+static char buf[BUFSIZE]; /* buffer for ungetch */
+static int bufp; /* next free position in buf */
+
+/* struct key wastes 4 bytes to align memory boundary.
+ * struct key already has optimized field layout.
+ * 8 byte pointer, then 4 byte int.
+ * We will always have 4 bytes leftover.
+ */
+static struct key {
+	char *word;
+	int count;
+} keytab[] = {
+	{"auto", 0},
+	{"break", 0},
+	{"case", 0},
+	{"char", 0},
+	{"continue", 0},
+	{"default", 0},
+	{"do", 0},
+	{"double", 0},
+	{"else", 0},
+	{"enum", 0},
+	{"extern", 0},
+	{"float", 0},
+	{"for", 0},
+	{"goto", 0},
+	{"if", 0},
+	{"int", 0},
+	{"long", 0},
+	{"register", 0},
+	{"return", 0},
+	{"short", 0},
+	{"sizeof", 0},
+	{"signed", 0},
+	{"static", 0},
+	{"struct", 0},
+	{"switch", 0},
+	{"typedef", 0},
+	{"union", 0},
+	{"unsigned", 0},
+	{"void", 0},
+	{"volatile", 0},
+	{"while", 0},
+};
+
+
+int getword(char *, int);
+struct key *binsearch(char *, struct key *, int);
+static int getch(void);
+static void ungetch(int);
+
+/* count C keywords; pointer version */
+int main(void)
+{
+	char word[MAXWORD];
+	struct key *p;
+	while (getword(word, MAXWORD) != EOF)
+		if (isalpha(word[0]))
+			if ((p=binsearch(word, keytab, NKEYS)) != NULL)
+				p->count++;
+	for (p = keytab; p < keytab + NKEYS; p++)
+		if (p->count > 0)
+			printf("%4d %s\n", p->count, p->word);
+	return 0;
+}
+
+/* getword: get next word or character from input */
+int getword(char *word, int lim)
+{
+	int c, getch(void);
+	void ungetch(int);
+	char *w = word;
+	while (isspace(c = getch()))
+		;
+	if (c != EOF)
+		*w++ = c;
+	if (!isalpha(c)) {
+		*w = '\0';
+		return c;
+	}
+	for ( ; --lim > 0; w++)
+		if (!isalnum(*w = getch())) {
+			ungetch(*w);
+			break;
+		}
+	*w = '\0';
+	return word[0];
+}
+
+/* binsearch: find word in tab[0]...tab[n-1] */
+struct key *binsearch(char *word, struct key *tab, int n)
+{
+	int cond;
+	struct key *low = &tab[0];
+	struct key *high = &tab[n];
+	struct key *mid;
+	while (low < high) {
+		mid = low + (high-low) / 2;
+		if ((cond = strcmp(word, mid->word)) < 0)
+			high = mid;
+		else if (cond > 0)
+			low = mid + 1;
+		else
+			return mid;
+	}
+	return NULL;
+}
+/* get a possibly pushed back character */
+static int getch(void)
+{
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+/* push character back on input */
+static void ungetch(int c)
+{
+	if (bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
